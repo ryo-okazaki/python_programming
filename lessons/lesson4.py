8def34458311a97a57ed5b8ad8c5ee8c013f3020
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-01 17:35:40 +0900

    lesson4

---
+++ lessons/lesson4.py
@@ -0,0 +1,42 @@
+print(r'C:\name\name')
+
+print("""
+line1
+line2
+line3
+""")
+
+print('##########')
+print("""\
+line1
+line2
+line3\
+""")
+print('##########')
+
+print('Hi.' * 3 + 'Mike.')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbbbbb')
+
+ss = 'aaaaaaaaaaaaaaaaaaaaaaaaaaa' \
+    'bbbbbbbbbbbbbbbbbbbbbbbbbbb'
+
+print(s)
+print(ss)
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print('##################')
+print(word[:2])
+print(word[2:])
+
+word = 'j' + word[1:]
+print(word[:])
+
+n = len(word)
+print(n)
