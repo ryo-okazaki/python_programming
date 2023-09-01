07f9e551abd0f43692caa4f9d65faed3d093d0bd
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-01 17:06:27 +0900

    lesson2

---
+++ lessons/lesson2.py
@@ -0,0 +1,14 @@
+num = 1
+name = 'Mike'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+# new_num = int(name)
+
+# print(new_num, type(new_num))
+
+num1: int = 1
+name1: str = '1'
