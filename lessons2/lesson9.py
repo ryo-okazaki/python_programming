14408bbe685c4a7da61b87ed2b3e6466b4fa0e4e
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:57:17 +0900

    section2 lesson9

---
+++ lessons2/lesson9.py
@@ -0,0 +1,17 @@
+some_list = [1, 2, 3, 4, 5]
+
+i = 0
+while i < len(some_list):
+    print(some_list[i])
+    i += 1
+
+for i in some_list: # 反復処理をするものをiterableという(またはイテレータ)
+    print(i)
+
+for s in 'abcde':
+    print(s)
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        continue
+    print(word)
