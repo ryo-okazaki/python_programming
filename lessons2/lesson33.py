2cbbbcd14db1281be6797a901f1e765f17bd8ff4
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 15:02:48 +0900

    section2 lesson33

---
+++ lessons2/lesson33.py
@@ -0,0 +1,15 @@
+# raise IndexError('test error')
+
+class UppercaseError(Exception):
+    pass
+
+def check():
+    words = ['APPLE', 'orange', 'banana']
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+
+try:
+    check()
+except UppercaseError as exc:
+    print('This is my fault. Go next')
