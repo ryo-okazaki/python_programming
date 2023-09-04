5e233baca4c0ec2a49851e9ba01be1ce39aed62e
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:54:45 +0900

    lesson12

---
+++ lessons/lesson12.py
@@ -0,0 +1,31 @@
+print('####################')
+
+num_tuple = (10, 20)
+print(num_tuple)
+
+x, y = num_tuple
+print(x, y)
+
+x, y = (10, 20)
+print(x, y)
+
+x, y = 10, 20
+print(x, y)
+
+i = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+
+print(i, j)
+
+a = 100
+b = 200
+print(a, b)
+
+a, b = b, a
+print(a, b)
+# 変数の入れ替え
+
+print('####################')
