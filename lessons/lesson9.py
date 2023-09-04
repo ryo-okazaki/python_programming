3700139996ba4226fd1a94ae066def5863003fea
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:54:23 +0900

    lesson9

---
+++ lessons/lesson9.py
@@ -0,0 +1,27 @@
+print('####################')
+
+r = [1, 2, 3, 4, 5, 1, 2, 3]
+print(r.index(3))
+print(r.index(3, 3))
+
+print(r.count(3))
+
+if 5 in r:
+    print('exist')
+
+r.sort()
+print(r)
+
+r.sort(reverse=True)
+
+r.reverse()
+print(r)
+
+s = 'My name is Mike.'
+to_split = s.split(' ')
+print(to_split)
+
+x = ' '.join(to_split)
+print(x)
+
+print('####################')
