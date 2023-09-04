863fcc57947ec84d9d8e887e4446928d82c1cb15
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 15:17:42 +0900

    section3 lesson3

---
+++ lesson_package/talk/animal.py
@@ -0,0 +1,7 @@
+from lesson_package.tools import utils
+
+def sing():
+    return 'aaaaasing'
+
+def cry():
+    return utils.say_twice('bbbbbcry')
---
+++ lessons3/lesson3.py
@@ -0,0 +1,8 @@
+from lesson_package.talk import human
+from lesson_package.talk import animal
+
+print(human.sing())
+print(human.cry())
+
+print(animal.sing())
+print(animal.cry())
