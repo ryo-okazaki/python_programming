2020cee29bf8801138291e41562d888e9b228161
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:55:00 +0900

    lesson14

---
+++ lessons/lesson14.py
@@ -0,0 +1,22 @@
+print('####################')
+
+d = {'x': 10, 'y': 20}
+print(d)
+print(type(d))
+
+print(d['x'])
+print(d['y'])
+
+d['x'] = 100
+print(d)
+
+d[1] = 11111
+print(d)
+
+dict(a=10, b=20)
+print(dict(a=10, b=20))
+
+dict([('a', 10), ('b', 20)])
+print(dict([('a', 10), ('b', 20)]))
+
+print('####################')
