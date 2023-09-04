e7eaa4630fca0817b8356d4d68ebb4c9f841d2b5
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:54:53 +0900

    lesson13

---
+++ lessons/lesson13.py
@@ -0,0 +1,14 @@
+print('####################')
+
+choose_from_to = ('A', 'B', 'C')
+
+answer = []
+answer.append('A')
+answer.append('C')
+
+print(choose_from_to)
+print(answer)
+
+# タプルは書き換えられないので、それを行ったときにエラーが出る
+
+print('####################')
