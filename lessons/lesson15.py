71ff5152db07ef51ba20c7a253c94c5db85a536c
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:55:08 +0900

    lesson15

---
+++ lessons/lesson15.py
@@ -0,0 +1,16 @@
+print('####################')
+
+d = {'x': 10, 'y': 20}
+
+d.keys()
+print(d.keys())
+d.values()
+print(d.values())
+
+d2 = {'x': 1000, 'j': 500}
+d.update(d2)
+# dにオーバーライド
+
+print('a' in d)
+
+print('####################')
