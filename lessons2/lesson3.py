06f133ca92c6f249758c50b0d04658ec863bf0a4
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 10:56:27 +0900

    section2 lesson3

---
+++ lessons2/lesson3.py
@@ -0,0 +1,21 @@
+
+x = 10
+
+if x < 0:
+    print('negative')
+elif x == 0:
+    print('zero')
+elif x == 10:
+    print('1000000')
+else:
+    print('positive')
+
+a = 5
+b = 10
+
+if a > 0:
+    print('a is positive')
+    if b > 0:
+        print('b is positive')
+
+# Pythonではインデントを揃えることでifの条件内の処理を範囲を決めている
