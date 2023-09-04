44fa930341dcfc05fb2b2f8d7e703579ea4fba86
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-04 15:20:05 +0900

    section3 lesson4

--- lessons3/lesson3.py
+++ lessons3/lesson3.py
@@ -1,5 +1,7 @@
-from lesson_package.talk import human
-from lesson_package.talk import animal
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+
+from lesson_package.talk import * # あまり推奨されない
 
 print(human.sing())
 print(human.cry())
