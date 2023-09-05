aaa502dfdb7d0fd73d34bf5f57f2a4b6e244471a
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-05 14:48:16 +0900

    section4 lesson2 クラス

--- lessons4/lesson2.py
+++ lessons4/lesson2.py
@@ -10,15 +10,37 @@ class ToyotaCar(Car): # Carを継承 引数を見る
         print('fast')
 
 class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=False):
+    def __init__(self, model='Model S',
+                 enable_auto_run=False,
+                 enable_auto_run2=False,
+                 passwd='123'):
         # self.model = model
         super().__init__(model)
-        self.enable_auto_run = enable_auto_run # tesra car だけの独自の引数
+        self.__enable_auto_run2 = None
+        self._enable_auto_run = enable_auto_run # tesra car だけの独自の引数
+        self.passwd = passwd
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run # 外部からアクセス可能
+
+    @property
+    def enable_auto_run2(self):
+        return self.__enable_auto_run2 # 外から見えないようにする クラス内からアクセス可能
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
+
     def auto_tun(self):
         print('auto run')
 
     def run(self):
         print('super fast')
+        print(self.__enable_auto_run2)
 
 car = Car()
 car.run()
@@ -30,7 +52,10 @@ toyota_car.run()
 print('###############')
 # 継承
 
-tesla_car = TeslaCar('Model S')
+tesla_car = TeslaCar('Model S', passwd='456')
 print(tesla_car.model)
+# print(tesla_car.enable_auto_run)
+print(tesla_car.enable_auto_run)
+tesla_car.enable_auto_run = True
 tesla_car.auto_tun()
 tesla_car.run()
