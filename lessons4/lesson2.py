91258088ce71ddca2d866ff92908ef0c6de0a0d2
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-05 11:13:41 +0900

    section4 lesson2 オーバーライド

--- lessons4/lesson2.py
+++ lessons4/lesson2.py
@@ -1,13 +1,20 @@
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+
     def run(self):
         print('run')
 
 class ToyotaCar(Car): # Carを継承 引数を見る
-    pass
+    def run(self):
+        print('fast')
 
 class TeslaCar(Car):
     def auto_tun(self):
         print('auto run')
+
+    def run(self):
+        print('super fast')
 
 car = Car()
 car.run()
