db81539c6a5a22d599598f311aa8c031ed0731b5
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-05 16:23:52 +0900

    section5 lesson ファイル読み込み

--- lessons5/lesson.py
+++ lessons5/lesson.py
@@ -6,8 +6,33 @@ print('test', file=f)
 print('My', 'name', 'is', 'Mike', sep='#', end='!', file=f)
 f.close()
 
+
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+
 # withは、インデント内の処理が終わったら勝手にcloseしてくれる
 with open('test.txt', 'w') as f:
     f.write('testestest\n')
     print('test', file=f)
     print('My', 'name', 'is', 'Mike', sep='#', end='!', file=f)
+    f.write(s)
+
+with open('test.txt', 'r') as f:
+    # print(f.read())
+
+    while True:
+        line = f.readline()
+        print(line, end='')
+        if not line:
+            break
+
+    # while True:
+    #     chunk = 2 # 2文字ずつ読み込む
+    #     line = f.read(chunk)
+    #     print(line)
+    #     if not line:
+    #         break
