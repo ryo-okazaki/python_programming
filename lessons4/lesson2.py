adc60a9e84676e4dbe8ad1ebee3383c4e644f30f
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-05 11:35:49 +0900

    section4 lesson2 親メソッドの呼び出し

--- lessons4/lesson2.py
+++ lessons4/lesson2.py
@@ -10,6 +10,10 @@ class ToyotaCar(Car): # Carを継承 引数を見る
         print('fast')
 
 class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run # tesra car だけの独自の引数
     def auto_tun(self):
         print('auto run')
 
@@ -20,11 +24,13 @@ car = Car()
 car.run()
 
 print('###############')
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 print('###############')
 # 継承
 
-tesla_car = TeslaCar()
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
 tesla_car.auto_tun()
 tesla_car.run()
