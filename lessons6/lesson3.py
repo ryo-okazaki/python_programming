c75b4ddc96e76b275e8b15b4d8b066cf43ac3496
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-06 16:55:54 +0900

    section6 lesson3 ログの設定

---
+++ lessons6/lesson3.py
@@ -0,0 +1,21 @@
+"""
+CRITICAL
+ERROR
+WARNING
+INFO
+DEBUG
+"""
+import logging
+
+logging.basicConfig(filename='test.log', level=logging.INFO)
+# ログのレベルを変える
+
+logging.critical('critical')
+logging.error('error')
+logging.warning('warning')
+logging.info('info')
+logging.debug('debug')
+
+logging.info('info {}'.format('test'))
+logging.info('info %s %s' % ('test', 'test2'))
+logging.info('info %s %s', 'test', 'test2')
