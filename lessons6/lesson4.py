fbb0c4920d00bb08ba3adf3ec2a938690a26c963
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-07 10:45:02 +0900

    section6 lesson4 フィルター

--- lessons6/lesson4.py
+++ lessons6/lesson4.py
@@ -12,10 +12,15 @@ from lessons6 import logtest
 
 logging.basicConfig(level=logging.INFO)
 
-logging.info('info')
+class NoPassFilter(logging.Filter):
+    def filter(self, record):
+        log_message = record.getMessage()
+        return 'password' not in log_message
 
 logger = logging.getLogger(__name__)
+logger.addFilter(NoPassFilter())
 logger.debug('debug')
+logger.debug('debug passsword = "test"')
 
 logtest.do_something()
 
