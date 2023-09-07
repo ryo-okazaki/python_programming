164532f1f59448361a647e520c7b23c15cb06448
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-07 21:02:06 +0900

    section10 lesson1 json

--- lessons10/lesson.py
+++ lessons10/lesson.py
@@ -49,3 +49,26 @@ for employee in root:
     for employ in employee:
         for person in employ:
             print(person.tag, person.text)
+
+import json
+j = {
+    'employee': [
+        {'id': '111', 'name': 'Mike'},
+        {'id': '222', 'name': 'Nancy'}
+    ]
+}
+
+print(j)
+print('#####################')
+# print(json.dumps(j))
+a = json.dumps(j)
+
+print('#####################')
+print(json.loads(a))
+print('#####################')
+
+with open('test.json', 'w') as f:
+    json.dump(j, f)
+
+with open('test.json', 'r') as f:
+    print(json.load(f))
