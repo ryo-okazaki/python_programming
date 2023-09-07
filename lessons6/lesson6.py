6a33d2bafb924157687599ca1d033b53c63ebf13
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-07 11:01:36 +0900

    section6 lesson6 ログの書き方

---
+++ lessons6/lesson6.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.error('Api call is failed.')
+
+logger.error({
+    'action': 'create',
+    'status': 'fail',
+    'message': 'Api call is failed.'
+})
