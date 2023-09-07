2255ed1d4da8690832469e8358745f3501615d90
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-07 11:46:16 +0900

    section7 lesson emailの添付ファイル送信

--- lessons7/lesson.py
+++ lessons7/lesson.py
@@ -1,4 +1,6 @@
 from email import message
+from email.mime import multipart
+from email.mime import text
 import smtplib
 
 smtp_host = 'smtp.gmail.com'
@@ -8,11 +10,23 @@ to_email = 'xxxxxi@gmail.com'
 username = 'xxxxx'
 password = 'xxxxxx'
 
-msg = message.EmailMessage()
-msg.set_content('Test email')
+# msg = message.EmailMessage()
+# msg.set_content('Test email')
+
+msg = multipart.MIMEMultipart()
+
 msg['Subject'] = 'Test email sub'
 msg['From'] = from_email
 msg['To'] = to_email
+
+msg.attach(text.MIMEText('Test email', 'plain'))
+
+with open('lesson.py', 'r') as f:
+    attachment = text.MIMEText(f.read(), 'plain')
+    attachment.add_header(
+        'Content-Disposition', 'attachment', filename='lesson.py'
+    )
+    msg.attach(attachment)
 
 server = smtplib.SMTP(smtp_host, smtp_port)
 server.ehlo() # SMTPサーバーに接続する
