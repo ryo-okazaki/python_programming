93b92794ed07fdf921121a53382e053e854fd15c
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-07 10:38:21 +0900

    section6 lesson4 ログのハンドラ

--- lessons6/logtest.py
+++ lessons6/logtest.py
@@ -3,6 +3,12 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG) # basicConfigよりも優先される
 
+h = logging.FileHandler('logtest.log')
+logger.addHandler(h) # ファイルに書き込まれるのはloggerだけ。
+# loggingはコンソールに出力される
+# lesson4で実行する
+
+
 def do_something():
     logger.info('from logtest.py')
     logger.debug('debug')
