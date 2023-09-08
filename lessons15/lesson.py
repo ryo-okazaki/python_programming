608a317c28ba7748cda05cddeeea205add899179
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-09 01:50:45 +0900

    section15 lesson デーモンスレッド

--- lessons15/lesson.py
+++ lessons15/lesson.py
@@ -16,13 +16,23 @@ def worker2(x, y=1):
     logging.debug(x)
     logging.debug(y)
     # print(threading.currentThread().getName(), 'start')
-    time.sleep(5)
+    time.sleep(2)
     logging.debug('end')
     # print(threading.currentThread().getName(), 'end')
 
+# if __name__ == '__main__':
+#     t1 = threading.Thread(name='rename worker1', target=worker1)
+#     t2 = threading.Thread(target=worker2, args=(100, ), kwargs={'y': 200})
+#     t1.start()
+#     t2.start()
+#     print('started')
+
 if __name__ == '__main__':
-    t1 = threading.Thread(name='rename worker1', target=worker1)
-    t2 = threading.Thread(target=worker2, args=(100, ), kwargs={'y': 200})
+    t1 = threading.Thread(target=worker1)
+    t1.setDaemon(True)
+    t2 = threading.Thread(target=worker2)
+    t2.setDaemon(True)
     t1.start()
     t2.start()
     print('started')
+    t1.join() # t1が終わるまで待つ 強制終了しない デーモン化した場合に記述する
