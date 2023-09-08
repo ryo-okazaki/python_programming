bc45bb61bba291419178fd83aca6ab622294c935
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-08 16:17:28 +0900

    section14 lesson2 テストコード

---
+++ lessons14/lesson2_test.py
@@ -0,0 +1,32 @@
+import unittest
+
+import lesson2
+
+class CalTest(unittest.TestCase):
+    def test_add_num_and_double(self):
+        cal = lesson2.Cal()
+        self.assertEquals(
+            cal.add_num_and_double(1, 1), 4
+        )
+        """
+        assertEqual(a, b)   a == b
+        assertNotEqual(a, b)    a != b
+        assertTrue(x)   bool(x) is True
+        assertFalse(x)  bool(x) is False
+        assertIs(a, b)  a is b
+        assertIsNot(a, b)   a is not b
+        assertIsNone(x) x is None
+        assertIsNotNone(x)  x is not None
+        assertIn(a, b)  a in b
+        assertNotIn(a, b)   a not in b
+        assertIsInstance(a, b)  isinstance(a, b)
+        assertNotIsInstance(a, b)   not isinstance(a, b)
+        """
+
+    def test_add_num_and_double_raise(self):
+        cal = lesson2.Cal()
+        with self.assertRaises(ValueError):
+            cal.add_num_and_double('1', '1')
+
+if __name__ == '__main__':
+    unittest.main()
