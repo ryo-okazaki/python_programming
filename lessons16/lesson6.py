5b6d00c08f8e4a29ac962b4fe19e14ccbedde82e
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-10 00:16:10 +0900

    section16 lesson6 プロセス間でのメモリ共有

---
+++ lessons16/lesson6.py
@@ -0,0 +1,24 @@
+import logging
+import multiprocessing
+import time
+
+logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+def f(num, arr):
+    num.value += 1.0
+    logging.debug(arr)
+    for i in range(len(arr)):
+        arr[i] *= 2
+
+if __name__ == '__main__':
+    num = multiprocessing.Value('f', 0.0)
+    arr = multiprocessing.Array('i', [1, 2, 3, 4, 5])
+
+    p1 = multiprocessing.Process(target=f, args=(num, arr))
+    p2 = multiprocessing.Process(target=f, args=(num, arr))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    logging.debug(num.value)
+    logging.debug(arr[:])
