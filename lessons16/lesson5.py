b66ad85d1dd176e0d310e5a82467acbea337b8d1
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-10 00:06:10 +0900

    section16 lesson5 パイプ

---
+++ lessons16/lesson5.py
@@ -0,0 +1,16 @@
+import logging
+import multiprocessing
+import time
+
+logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+def f(conn):
+    conn.send(['test'])
+    time.sleep(5)
+    conn.close()
+
+if __name__ == '__main__':
+    parent_conn, child_conn = multiprocessing.Pipe()
+    p = multiprocessing.Process(target=f, args=(child_conn,))
+    p.start()
+    logging.debug(child_conn.recv())
