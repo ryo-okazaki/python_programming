8624ef073c9c725189ab84aaafece1a0427c40f2
Ryo Okazaki <okazaki3905@gmail.com>
2023-09-10 00:40:38 +0900

    section16 lesson9 concurrent.futures

---
+++ lessons16/lesson9.py
@@ -0,0 +1,31 @@
+import threading
+import multiprocessing
+
+import concurrent.futures
+import logging
+import time
+
+# logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+def worker(x, y):
+    logging.debug('start')
+    r = x * y
+    logging.debug(r)
+    logging.debug('end')
+    return r
+
+def main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: # max_workersでスレッドの数を制限できる
+        f1 = executor.submit(worker, 2, 5)
+        f2 = executor.submit(worker, 2, 5)
+        logging.debug(f1.result())
+        logging.debug(f2.result())
+
+        args = [[2, 5], [3, 10], [4, 15]]
+        r = executor.map(worker, *args)
+        logging.debug(r)
+        logging.debug([i for i in r])
+
+if __name__ == '__main__':
+    main()
